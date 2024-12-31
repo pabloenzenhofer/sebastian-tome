e95343562cf8f61b459bb6503cd37deaaed110f4
pabloenzenhofer <pabloenzenhofer@gmail.com>
2024-12-31 10:07:35 -0300

    Initial commit with project structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/variables.css'
 import './index.css'
-import App from './App'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
